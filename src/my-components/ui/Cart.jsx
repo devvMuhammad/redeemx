b3d7c0597d0dd11d830e53de122c5a9fbeddba4f
muhammad <muhammadaljoufi@gmail.com>
2023-08-24 20:22:39 +0500

    fixed cart

--- src/my-components/ui/Cart.jsx
+++ src/my-components/ui/Cart.jsx
@@ -1,21 +1,24 @@
 import Link from "next/link";
 
 export default function Cart({ showSidebar }) {
-  <div
-    className={`flex flex-col justify-center p-4 mt-4 text-white ${
-      showSidebar
-        ? "transition-opacity duration-200 ease-linear opacity-100"
-        : "opacity-0"
-    }`}
-  >
-    <p className="font-bold text-xl text-left">
-      Cart
-      <Link
-        href=""
-        className="inline-block ml-2 text-sm font-normal hover:underline hover:translate-y-[-1px]"
-      >
-        ( View full here )
-      </Link>
-    </p>
-  </div>;
+  return (
+    <div
+      className={`flex flex-col justify-center p-4 mt-4 text-white ${
+        showSidebar
+          ? "transition-opacity duration-400 ease-linear opacity-100"
+          : "opacity-0"
+      }`}
+    >
+      <p className="font-bold text-xl text-left">
+        Cart
+        <Link
+          href=""
+          className="inline-block ml-2 text-sm font-normal hover:underline hover:translate-y-[-1px]"
+        >
+          ( View full here )
+        </Link>
+      </p>
+      <hr className="mt-4" />
+    </div>
+  );
 }
