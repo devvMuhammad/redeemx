5b18c31a68bcc563ac058b71ad5da5a3ada5fa34
muhammad <muhammadaljoufi@gmail.com>
2023-08-24 21:57:32 +0500

    text changes in home page

--- src/my-components/header/HomePage.jsx
+++ src/my-components/header/HomePage.jsx
@@ -1,10 +1,10 @@
 function HomePage() {
   return (
     <div className="flex flex-col  justify-center gap-10 text-center flex-1 bg-black text-white p-4 md:p-10 lg:p-20">
-      <h1 className="text-4xl md:text-5xl font-extrabold">
+      <h1 className="text-4xl md:text-6xl font-extrabold tracking-wide">
         Discover Your Perfect Tech Match
       </h1>
-      <p className="text-md md:text-xl">
+      <p className="text-md md:text-xl text-gray-300">
         Discover an exquisite range of laptops that boost productivity,
         smartphones that keep you connected in style, and gift cards that unlock
         a world of possibilities – all designed to elevate your modern lifestyle
