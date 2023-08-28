c40a389fdf63edc58d2a2d3972ebcb2ec02937c4
muhammad <muhammadaljoufi@gmail.com>
2023-08-28 14:54:35 +0500

    made navbar stick

--- src/my-components/header/nav/NavBar.jsx
+++ src/my-components/header/nav/NavBar.jsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 function NavBar() {
   return (
-    <nav className="h-16 p-6 lg:p-10 flex justify-between items-center bg-black text-white opacity-95">
+    <nav className="sticky top-0 h-16 p-6 lg:p-10 flex justify-between items-center bg-black text-white opacity-95 border-b-2 border-solid border-b-gray-600">
       <Hamburger />
       <div className="flex items-center gap-12">
         <Link className="text-3xl font-extrabold" href="/">
