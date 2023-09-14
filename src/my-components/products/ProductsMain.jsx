57376da18c58f3ba9775a5680abf8fb588685137
muhammad <muhammadaljoufi@gmail.com>
2023-09-14 10:41:00 +0500

    added main products

---
+++ src/my-components/products/ProductsMain.jsx
@@ -0,0 +1,35 @@
+import { Button } from "@/components/ui/button";
+
+export default function ProductsMain() {
+  return (
+    <div className="p-2 flex-1 grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-4 gap-x-1 b-red-500">
+      {Array.from({ length: 9 }, (_, i) => (
+        <div className="p-4 flex flex-col gap-4 border border-gray-600 rounded-lg ">
+          <div className="h-[200px] bg-white rounded-md"></div>
+
+          {/* Link in future */}
+          <h2>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+            repellat nemo
+          </h2>
+          {/* PRICES */}
+          <div className="w-full flex gap-3">
+            <span className="font-semibold text-xl tracking-wider mr-6">
+              $2000
+            </span>
+            <span className="font-semibold text-xl tracking-wider line-through">
+              $1100
+            </span>
+          </div>
+
+          <div>
+            {/* Agar variant huwa to add kardena */}
+            <Button className="font-bold tracking-wide" size="sm">
+              Add to Cart
+            </Button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
