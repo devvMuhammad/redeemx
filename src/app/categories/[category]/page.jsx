f590141229f9f1486c28533a2b9599b813787cfa
muhammad <muhammadaljoufi@gmail.com>
2023-09-14 10:42:24 +0500

    minor cleaning

--- src/app/categories/[category]/page.jsx
+++ src/app/categories/[category]/page.jsx
@@ -3,6 +3,7 @@ import Section from "@/lib/Section";
 import { toTitleCase } from "@/lib/utils";
 import Filters from "@/my-components/filters/Filters";
 import ProductsHeader from "@/my-components/products/ProductsHeader";
+import ProductsMain from "@/my-components/products/ProductsMain";
 
 export default function Category({ params: { category } }) {
   const titledCategory = toTitleCase(category);
@@ -22,35 +23,7 @@ export default function Category({ params: { category } }) {
           {/* PRODUCTS HEADER */}
           <ProductsHeader />
           {/* MAIN PRODUCTS */}
-          <div className="p-2 flex-1 grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 b-red-500">
-            {Array.from({ length: 9 }, (_, i) => (
-              <div className="flex flex-col gap-4 border rounded-xl p-2">
-                <div className="h-[200px] bg-white"></div>
-
-                {/* Link in future */}
-                <h2>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Corrupti repellat nemo
-                </h2>
-                {/* PRICES */}
-                <div className="w-full flex gap-3">
-                  <span className="font-semibold text-xl tracking-wider mr-6">
-                    $2000
-                  </span>
-                  <span className="font-semibold text-xl tracking-wider line-through">
-                    $1100
-                  </span>
-                </div>
-
-                <div>
-                  {/* Agar variant huwa to add kardena */}
-                  <Button className="font-bold tracking-wide" size="sm">
-                    Add to Cart
-                  </Button>
-                </div>
-              </div>
-            ))}
-          </div>
+          <ProductsMain />
         </div>
       </Section>
     </main>
--- src/my-components/products/ProductsHeader.jsx
+++ src/my-components/products/ProductsHeader.jsx
@@ -1,19 +1,33 @@
 import React from "react";
-import Grid2x2 from "../ui/Grid2x2";
-import Grid3x3 from "../ui/Grid3x3";
+
 import NumberSelect from "./NumberSelect";
 import SortSelect from "./SortSelect";
 import FiltersSheet from "./FiltersSheet";
 
 function ProductsHeader() {
   return (
-    <div className="flex justify-between items-center py-2 lg:px-2 border-b border-gray-500">
+    <div className="flex justify-between items-center py-2 lg:px-2 ">
       <p className="hidden md:block text-lg">
         Showing <strong>10</strong> of <strong>25</strong> Items
       </p>
-      <div className="hidden md:flex gap-2 items-center">
-        <Grid2x2 />
-        <Grid3x3 />
+      <div className="hidden md:flex gap-4 items-center">
+        {/* <Grid2x2 />
+        <Grid3x3 /> */}
+        <div className="grid grid-cols-2 gap-1 group cursor-pointer">
+          {Array.from({ length: 4 }).map(() => (
+            <div className="w-2 h-2 rounded-full bg-white  group-hover:bg-gray-500 transition-all duration-200"></div>
+          ))}
+        </div>
+        <div className="grid grid-cols-3 gap-1 group cursor-pointer">
+          {Array.from({ length: 9 }).map(() => (
+            <div className="w-[5.5px] h-[5.5px] rounded-full bg-white  group-hover:bg-gray-500 transition-all duration-200"></div>
+          ))}
+        </div>
+        <div className="hidden lg:grid grid-cols-4 gap-1 group cursor-pointer">
+          {Array.from({ length: 12 }).map(() => (
+            <div className="w-[5px] h-[5px] rounded-full bg-white  group-hover:bg-gray-500  transition-all duration-200"></div>
+          ))}
+        </div>
       </div>
       <div className="flex gap-2 items-center justify-between w-full md:w-auto">
         <div className="flex gap-2">
