3b51af8c4fe154110b56eaf630fcb3c54562bad3
muhammad <muhammadaljoufi@gmail.com>
2023-09-20 01:11:49 +0500

    changed skeleton size

--- src/my-components/ui/ProductsSkeletion.jsx
+++ src/my-components/ui/ProductsSkeletion.jsx
@@ -2,11 +2,11 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function ProductsSkeleton() {
   return (
-    <div className="flex flex-col bg-sy-500">
+    <div className="flex flex-col">
       <div className="flex justify-between items-center py-2 lg:px-2 ">
         <Skeleton className="w-40 h-10" />
         <Skeleton className="w-32 h-10" />
-        <Skeleton className="w-40 h-10" />
+        <Skeleton className="w-60 h-10" />
       </div>
 
       <div
