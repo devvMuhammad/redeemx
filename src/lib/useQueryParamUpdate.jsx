eb855391a47d0be786ca70b6e24f82698aeda6e3
muhammad <muhammadaljoufi@gmail.com>
2023-09-24 11:10:17 +0500

    added update and delete functionality

--- src/lib/useQueryParamUpdate.jsx
+++ src/lib/useQueryParamUpdate.jsx
@@ -1,7 +1,22 @@
 // useQueryParamUpdater.js
 import { useSearchParams, useRouter, usePathname } from "next/navigation";
 import { useCallback } from "react";
-import { createQueryString } from "@/lib/utils";
+
+const createQueryString = (name, value, add = true, searchParams) => {
+  const params = new URLSearchParams(searchParams);
+  const existingParam = params.get(name);
+  // if already a param exists, then add to it
+  //e.g brand="Apple" --> brand="Apple,HP";
+  params.set(name, existingParam && add ? `${existingParam}.${value}` : value);
+  console.log(searchParams.toString());
+  return params.toString();
+};
+const deleteQueryString = (name, searchParams) => {
+  const params = new URLSearchParams(searchParams);
+  params.delete(name);
+  console.log(params.toString());
+  return params.toString();
+};
 
 export default function useQueryParamUpdate() {
   const router = useRouter();
@@ -9,12 +24,29 @@ export default function useQueryParamUpdate() {
   const searchParams = useSearchParams();
 
   const updateQueryParam = useCallback(
-    (paramName, value) => {
-      const queryString = createQueryString(paramName, value, searchParams);
+    (paramName, value, add) => {
+      const queryString = createQueryString(
+        paramName,
+        value,
+        add,
+        searchParams
+      );
       router.push(pathname + "?" + queryString);
     },
     [pathname, router, searchParams]
   );
 
-  return { searchParams, updateSearchParams: updateQueryParam };
+  const deleteQueryParam = useCallback(
+    (paramName) => {
+      const queryString = deleteQueryString(paramName, searchParams);
+      router.push(pathname, "", { scroll: false });
+    },
+    [pathname, router, searchParams]
+  );
+
+  return {
+    searchParams,
+    updateSearchParams: updateQueryParam,
+    deleteSearchParams: deleteQueryParam,
+  };
 }
