f794a8f11062e596729c099df131a77a19375ac6
muhammad <muhammadaljoufi@gmail.com>
2023-10-29 14:14:37 +0500

    removed api folder

--- src/app/api/filters/route.js
+++
@@ -1,6 +0,0 @@
-import categories from "@/my-components/nav/categories";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return NextResponse.json({ data: categories }, { status: 200 });
-}
