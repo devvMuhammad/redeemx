61d30e682d183c998f64077e484425541a4bec35
muhammad <muhammadaljoufi@gmail.com>
2023-12-02 01:19:22 +0500

    separated product into its own component

---
+++ src/my-components/products/Product.jsx
@@ -0,0 +1,33 @@
+import { Button } from "@/components/ui/button";
+
+export default function Product({ product: { name, discount, price }, grid }) {
+  return (
+    <div
+      key={name}
+      className="flex flex-col p-4 gap-4 border border-gray-600 rounded-lg "
+    >
+      <div
+        className={`${
+          +grid === 2 ? "h-[300px]" : "h-[200px]"
+        }  bg-gray-500 rounded-md`}
+      ></div>
+
+      {/* Link in future */}
+      <h2 className={`flex-1 ${+grid < 4 && "text-xl"}`}>{name}</h2>
+      {/* PRICES */}
+      <div className="space-y-4">
+        <div className={`flex gap-8 flex-1 ${+grid < 4 && "text-xl"}`}>
+          <span className="font-semibold text-lg tracking-wider">${price}</span>
+          <span className="font-semibold text-lg tracking-wider line-through">
+            {discount && `$${discount + price}`}
+          </span>
+        </div>
+
+        {/* Agar variant huwa to add kardena */}
+        <Button className="font-bold tracking-wide" size="sm">
+          Add to Cart
+        </Button>
+      </div>
+    </div>
+  );
+}
--- src/my-components/products/ProductsMain.jsx
+++ src/my-components/products/ProductsMain.jsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import Product from "./Product";
 
 export default function ProductsMain({ grid, products }) {
   const gridObj = {
@@ -11,35 +12,7 @@ export default function ProductsMain({ grid, products }) {
       className={`p-2 flex-1 grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 ${gridObj[grid]} gap-y-4 b-red-500`}
     >
       {products.map((product) => (
-        <div
-          key={product.name}
-          className="flex flex-col p-4 gap-4 border border-gray-600 rounded-lg "
-        >
-          <div
-            className={`${
-              +grid === 2 ? "h-[300px]" : "h-[200px]"
-            }  bg-gray-500 rounded-md`}
-          ></div>
-
-          {/* Link in future */}
-          <h2 className={`flex-1 ${+grid < 4 && "text-xl"}`}>{product.name}</h2>
-          {/* PRICES */}
-          <div className="space-y-4">
-            <div className={`flex gap-8 flex-1 ${+grid < 4 && "text-xl"}`}>
-              <span className="font-semibold text-lg tracking-wider">
-                ${product.price}
-              </span>
-              <span className="font-semibold text-lg tracking-wider line-through">
-                {product.discount && `$${product.discount + product.price}`}
-              </span>
-            </div>
-
-            {/* Agar variant huwa to add kardena */}
-            <Button className="font-bold tracking-wide" size="sm">
-              Add to Cart
-            </Button>
-          </div>
-        </div>
+        <Product key={product.name} product={product} grid={grid} />
       ))}
     </div>
   );
