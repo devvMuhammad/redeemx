8646b8af822de0092774edbfe9ab5d3ca0c5e956
muhammad <muhammadaljoufi@gmail.com>
2024-01-27 13:53:47 +0500

    a minor corrction

--- src/components/products/ProductsMain.jsx
+++ src/components/products/ProductsMain.jsx
@@ -6,7 +6,7 @@ export default function ProductsMain({ products }) {
       className={`p-2 flex-1 grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-2 gap-y-4 b-red-500`}
     >
       {products.map((product) => (
-        <Product key={product.name} product={product} grid={grid} />
+        <Product key={product.name} product={product} />
       ))}
     </div>
   );
