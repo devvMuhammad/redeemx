5bf4494cf8157da0a9791b3e2aaad51540b8eb32
muhammad <muhammadaljoufi@gmail.com>
2024-01-27 13:53:29 +0500

    grid param correction

--- src/components/products/ProductsMain.jsx
+++ src/components/products/ProductsMain.jsx
@@ -1,9 +1,9 @@
 import Product from "./Product";
 
-export default function ProductsMain({ grid, products }) {
+export default function ProductsMain({ products }) {
   return (
     <div
-      className={`p-2 flex-1 grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 gap-y-4 b-red-500`}
+      className={`p-2 flex-1 grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-2 gap-y-4 b-red-500`}
     >
       {products.map((product) => (
         <Product key={product.name} product={product} grid={grid} />
