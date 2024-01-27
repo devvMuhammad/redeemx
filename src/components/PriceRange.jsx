50c96f9cd398ff364b3f1def43875d9ca8c0669a
muhammad <muhammadaljoufi@gmail.com>
2024-01-27 12:39:02 +0500

    added a pending state to the price range

--- src/components/PriceRange.jsx
+++ src/components/PriceRange.jsx
@@ -4,11 +4,12 @@ import { Input } from "@/components/ui/input";
 import { Slider } from "@/components/ui/slider";
 import useCustomSearchParams from "@/hooks/useCustomSearchParams";
 import useQueryParamUpdate from "@/hooks/useQueryParamUpdate";
-import { useState } from "react";
+import { useState, useTransition } from "react";
 export default function PriceRange() {
   const { updateSearchParams } = useQueryParamUpdate();
   const { price } = useCustomSearchParams();
   const [priceRange, setPriceRange] = useState([price[0], price[1]]);
+  const [isPending, startTransition] = useTransition();
   return (
     <>
       <Slider
@@ -27,7 +28,9 @@ export default function PriceRange() {
           value={priceRange[0]}
           onChange={(e) => {
             const value = Number(e.target.value);
-            setPriceRange([value, priceRange[1]]);
+            startTransition(() => {
+              setPriceRange([value, priceRange[1]]);
+            });
           }}
         />
         <span>-</span>
@@ -40,21 +43,26 @@ export default function PriceRange() {
           value={priceRange[1]}
           onChange={(e) => {
             const value = Number(e.target.value);
-            setPriceRange([priceRange[0], value]);
+            startTransition(() => {
+              setPriceRange([priceRange[0], value]);
+            });
           }}
         />
       </div>
       <div className="w-full flex justify-end">
         <Button
+          disabled={isPending}
           size="sm"
           className="font-bold tracking-wide right-0 inline-flex"
           onClick={() => {
-            updateSearchParams(
-              "price",
-              `${priceRange[0]}-${priceRange[1]}`,
-              false,
-              true
-            );
+            startTransition(() => {
+              updateSearchParams(
+                "price",
+                `${priceRange[0]}-${priceRange[1]}`,
+                false,
+                true
+              );
+            });
           }}
         >
           Apply Price
