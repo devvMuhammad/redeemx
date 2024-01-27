993896165eed88c84f7a146fd9d45056a43a88fd
muhammad <muhammadaljoufi@gmail.com>
2024-01-27 13:51:59 +0500

    removed the grid param

--- src/components/products/ProductsMain.jsx
+++ src/components/products/ProductsMain.jsx
@@ -1,15 +1,9 @@
-import { Button } from "@/components/ui/button";
 import Product from "./Product";
 
 export default function ProductsMain({ grid, products }) {
-  const gridObj = {
-    2: "lg:grid-cols-2 gap-x-8",
-    3: "lg:grid-cols-3 gap-x-6",
-    4: "lg:grid-cols-4 gap-x-2",
-  };
   return (
     <div
-      className={`p-2 flex-1 grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 ${gridObj[grid]} gap-y-4 b-red-500`}
+      className={`p-2 flex-1 grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 gap-y-4 b-red-500`}
     >
       {products.map((product) => (
         <Product key={product.name} product={product} grid={grid} />
