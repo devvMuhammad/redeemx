b6d62627b0fc38658e6ec580ebc215429ca5561b
devvMuhammad <muhammadaljoufi@gmail.com>
2024-02-05 22:49:08 +0500

    added temporary signIn and signOut button

--- src/components/nav/UserProfile.jsx
+++ src/components/nav/UserProfile.jsx
@@ -1,10 +1,23 @@
+"use client";
 import Link from "next/link";
 import { Button } from "../ui/button";
+import { signIn, useSession } from "next-auth/react";
+import { signOut } from "next-auth/react";
 
 export default function UserProfile() {
-  return (
-    <Link href="/signin">
-      <Button size="sm">Sign In</Button>
-    </Link>
+  const { data: session } = useSession();
+  // signIn("github");
+
+  // console.log(session);
+  return session ? (
+    <Button size="sm" onClick={signOut}>
+      Sign Out
+    </Button>
+  ) : (
+    // <Link href="/signin">
+    <Button size="sm" onClick={() => signIn("github")}>
+      Sign In
+    </Button>
+    // </Link>
   );
 }
