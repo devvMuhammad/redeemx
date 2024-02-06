a7cf716a9f1834d46ca6ae4849c98db210c6ebd9
devvMuhammad <muhammadaljoufi@gmail.com>
2024-02-06 08:25:18 +0500

    implemented sign ins and removed facebook

--- src/components/SocialLogins.jsx
+++ src/components/SocialLogins.jsx
@@ -6,14 +6,14 @@ import { signIn } from "next-auth/react";
 export default function SocialLogins() {
   return (
     <div className="flex justify-between mb-6">
-      <Button className="flex-1 mr-2 bg-black hover:bg-white/10 border-2 border-gray-800  text-white">
+      <Button
+        onClick={() => signIn("google", { callbackUrl: "/" })}
+        className="flex-1 mr-2 bg-black hover:bg-white/10 border-2 border-gray-800  text-white"
+      >
         <LucideChrome className="mr-2" />
         Google{"\n              "}
       </Button>
-      <Button className="flex-1 mx-2 bg-black hover:bg-white/10 border-2 border-gray-800  text-white">
-        <Facebook className="mr-2" />
-        Facebook{"\n              "}
-      </Button>
+
       <Button
         onClick={() => signIn("github", { callbackUrl: "/" })}
         className="flex-1 ml-2 bg-black hover:bg-white/10 border-2 border-gray-800  text-white"
