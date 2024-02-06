1d7a9c734d8a3a23e097e36e36bca56d261539a7
devvMuhammad <muhammadaljoufi@gmail.com>
2024-02-06 08:26:02 +0500

    added user profiel images after signin

--- src/components/nav/UserProfile.jsx
+++ src/components/nav/UserProfile.jsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import { Button } from "../ui/button";
 import { signIn, useSession } from "next-auth/react";
 import { signOut } from "next-auth/react";
+import { User2Icon } from "lucide-react";
+import Image from "next/image";
 
 export default function UserProfile() {
   const { data: session } = useSession();
@@ -14,16 +16,30 @@ export default function UserProfile() {
   - on sign in, redirect to the main page and DISPLAY THE USER DATA (name and the profile picture)
   - are there any protected routes? I guess not
   */
-  console.log(session);
-  return session ? (
-    <Button size="sm" onClick={signOut}>
-      Sign Out
-    </Button>
-  ) : (
-    <Link href="/signin">
-      <Button size="sm" onClick={() => signIn("github")}>
-        Sign In
-      </Button>
-    </Link>
+  // console.log(session);
+  return (
+    <div className="flex gap-2">
+      {session ? (
+        <>
+          <Button size="sm" onClick={signOut}>
+            Sign Out
+          </Button>
+          <Image
+            className="rounded-full"
+            width={40}
+            height={32}
+            src={session.user.image}
+          />
+        </>
+      ) : (
+        <>
+          <Link href="/signin">
+            {/* <Button size="sm" onClick={() => signIn("github")}> */}
+            <Button size="sm">Sign In</Button>
+          </Link>
+          <User2Icon className="p-1 h-8 w-8 border-2 border-white rounded-full" />
+        </>
+      )}
+    </div>
   );
 }
