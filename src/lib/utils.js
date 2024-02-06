783f798bb54077481cf8f6ddf7bd0233a6410a25
devvMuhammad <muhammadaljoufi@gmail.com>
2024-02-06 21:25:28 +0500

    added a util function to generate unqiue ids

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -19,3 +19,23 @@ export function toTitleCase(str) {
 export function toCamelCase(str) {
   return str.toLowerCase().split(" ").join("-");
 }
+
+export function generateCustomerID() {
+  // Function to generate a random string of a specified length
+  const generateRandomString = (length) => {
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let result = "";
+
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      result += characters.charAt(randomIndex);
+    }
+
+    return result;
+  };
+
+  // Generate the random customer ID
+  const randomCustomerID = generateRandomString(6);
+
+  return randomCustomerID;
+}
