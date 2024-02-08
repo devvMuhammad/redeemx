6a3863d92a397be35521d684da38f02a619c7436
devvMuhammad <muhammadaljoufi@gmail.com>
2024-02-08 20:44:43 +0500

    refactored product page route

---
+++ src/app/(lobby)/categories/[category]/loading.jsx
@@ -0,0 +1,5 @@
+import ProductsSkeleton from "@/components/ui/ProductsSkeletion";
+
+export default function Loading() {
+  return <ProductsSkeleton />;
+}
--- src/app/(lobby)/categories/[category]/page.jsx
+++ src/app/(lobby)/categories/[category]/page.jsx
@@ -1,8 +1,6 @@
 import { toCamelCase, toTitleCase } from "@/lib/utils";
 import categories from "@/components/nav/categories";
 import Products from "@/components/products/Products";
-import ProductsSkeleton from "@/components/ui/ProductsSkeletion";
-import { Suspense } from "react";
 
 export async function generateStaticParams() {
   return categories.map((category) => ({
@@ -12,26 +10,5 @@ export async function generateStaticParams() {
 
 export default async function Category({ params: { category }, searchParams }) {
   const titledCategory = toTitleCase(category);
-  // const categories = await getCategories();
-  return (
-    <>
-      {/* FILTERS */}
-      {/* <Suspense fallback={<FiltersSkeleton />}>
-        <FiltersServer titledCategory={titledCategory} />
-      </Suspense> */}
-
-      {/* PRODUCTS */}
-      <Suspense
-        // key={[
-        //   searchParams.brand,
-        //   searchParams.price,
-        //   searchParams.sort,
-        //   searchParams.page,
-        // ]}
-        fallback={<ProductsSkeleton />}
-      >
-        <Products searchParams={searchParams} />
-      </Suspense>
-    </>
-  );
+  return <Products searchParams={searchParams} category={category} />;
 }
